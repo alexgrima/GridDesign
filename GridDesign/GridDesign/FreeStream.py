f9f3334f6f17262b71e2520d2c96c404af513646
alexgrima <alex@grima.se>
2016-03-16 15:36:27 -0700

    Updating the template

--- GridDesign/GridDesign/FreeStream.py
+++ GridDesign/GridDesign/FreeStream.py
@@ -11,37 +11,70 @@ class Freestream(object):
         calculations. If field value is None the internal value can 
         still recieve a value through calculations.
         """
+        # Ratio setting
+        self.ratio = 0.9            # Ratio value for V*t/x < 1
+
         # Field variabels
-        self._velocity = None          # Velocity [Lunit/Tunit]
-        self._velocity_ = None         # Internal
-        self._flow_distance = None     # Distance of flow travel [Lunit]
-        self._flow_distance_ = None    # Internal
+        self.velocity = None          # Velocity [Lunit/Tunit]
+        self._velocity = None         # Internal
+        self.flow_distance = None     # Distance of flow travel [Lunit]
+        self._flow_distance = None    # Internal
 
         # Time variabels
-        self._delta_t = None          # Time delta [Tunit]
-        self._delta_t_ = None         # Internal
-        self._time_interval = None    # Simulation time interval [Tunit]
-        self._time_interval_ = None   # Internal
-        self._time_steps = None       # Number of steps in interval []
-        self._time_steps_ = None      # Internal
+        self.delta_t = None          # Time delta [Tunit]
+        self._delta_t = None         # Internal
+        self.time_interval = None    # Simulation time interval [Tunit]
+        self._time_interval = None   # Internal
+        self.time_steps = None       # Number of steps in interval []
+        self._time_steps = None      # Internal
 
         # Grid variabels
-        self._delta_x = None          # Distance delta [Lunit]
-        self._delta_x_ = None         # Internal
-        self._reference_length = None # Reference length [Lunit]
-        self._reference_length_ = None# Internal
-        self._number_of_nodes = None  # Num nodes along reference []
-        self._number_of_nodes_ = None # Internal
+        self.delta_x = None          # Distance delta [Lunit]
+        self._delta_x = None         # Internal
+        self.reference_length = None # Reference length [Lunit]
+        self._reference_length = None# Internal
+        self.number_of_nodes = None  # Num nodes along reference []
+        self._number_of_nodes = None # Internal
 
         return super(Freestream, self).__init__()
 
-    # Velocity set::get
-    def set_velocity(self, velocity):
-        self._velocity = velocity
+    """Different variabel calculations.    
+        # Values should be defined as floats
+        # Add error handling
+    """
+    # velocity
+    # Calculate velocity from delta_t and delta_x
+    def _calc_velocity_deltas(self):
+        self._velocity = self.ratio*(self._delta_x/self._delta_t)
 
-    def get_velocity(self):
-        return self._velocity_
+    # Calculate velocity from flow distance and time interval
 
-    # Flow distance set::get
-    def set_flow_distance(self, flow_distance):
-        self._flow_distance = flow_distance
+    # delta_t
+    # Calculate delta_t from time interval and number of steps
+    def _calc_delta_t_from_time_and_steps(self):
+        self._delta_t = self._time_interval/self._time_steps
+
+    # Caluclate delta_t from velocity and delta_x
+    def _calc_delta_t_from_velocity(self):
+        self._delta_t = self._delta_x/self._velocity
+
+    # delta_x
+    # Calculate delta_x from reference length and number of nodes
+
+    # Calculate delta_x from velocity and delta_t
+    
+    # time_interval
+    # Calculate time from flow distance and velocity
+    def _calc_time_from_flow_distance(self):
+        self._time_interval = self._flow_distance/self._velocity
+
+    # Calculate time from delta_t and number of steps
+    
+    # time_steps
+    # Calculate number of steps from delta_t and time interval
+       
+    # ref_length
+    # Calculate ref_length from delta_x and number of nodes
+
+    # number_of_nodes
+    # Calculate number_of_nodes from delta_X and reference length 
