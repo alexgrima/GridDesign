b3a701100a4d1cb178a0a079dfcd805ea9515ed1
alexgrima <alex@grima.se>
2016-03-18 17:02:09 -0700

    Basic consol app imlemented

---
+++ GridDesign/GridDesign/GridDesign.py
@@ -0,0 +1,38 @@
+from Freestream import Freestream
+
+# Function that sets the value to the variabel specified
+def set_variabel(freestream, variable, value):
+    # velocity
+    if str(variable).lower() in "v":
+        freestream.set_velocity(float(value))
+    elif str(variable).lower() in "fd":
+        freestream.set_flow_distance(float(value))
+    elif str(variable).lower() in "dt":
+        freestream.set_delta_t(float(value))
+    elif str(variable).lower() in "dx":
+        freestream.set_delta_x(float(value))
+    elif str(variable).lower() in "ti":
+        freestream.set_time_interval(float(value))
+    elif str(variable).lower() in "ts":
+        freestream.set_time_step(float(value))
+    elif str(variable).lower() in "rl":
+        freestream.set_reference_length(float(value))
+    elif str(variable).lower() in "nn":
+        freestream.set_number_of_nodes(float(value))
+
+
+# main function
+def main():
+    # Generate instance of freestream calculator
+    freestream_calc = Freestream()
+
+    # Inifinate loop
+    while True:
+        print("\n" +str(freestream_calc))
+        print("What variable would you like to change?")
+        print("V, FD, dt, dx, ti, ts, rl, nn")
+        choise = raw_input("Choise: ")
+        value = raw_input("Value: ")
+        set_variabel(freestream_calc, choise, value)
+
+main()
