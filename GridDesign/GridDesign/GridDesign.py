7eda63ebc3040b6fca37f5870728f7f32f2b21b0
alexgrima <alex@grima.se>
2016-03-19 09:55:18 -0700

    consol app working

--- GridDesign/GridDesign/GridDesign.py
+++ GridDesign/GridDesign/GridDesign.py
@@ -2,24 +2,30 @@ from Freestream import Freestream
 
 # Function that sets the value to the variabel specified
 def set_variabel(freestream, variable, value):
+    # If value empty set to None
+    if str(value) == "":
+        value = None
+    # Set value
     if str(variable).lower() == "":
         pass
     elif str(variable).lower() in "v":
-        freestream.set_velocity(float(value))
+        freestream.set_velocity(value)
     elif str(variable).lower() in "fd":
-        freestream.set_flow_distance(float(value))
+        freestream.set_flow_distance(value)
     elif str(variable).lower() in "dt":
-        freestream.set_delta_t(float(value))
+        freestream.set_delta_t(value)
     elif str(variable).lower() in "dx":
-        freestream.set_delta_x(float(value))
+        freestream.set_delta_x(value)
     elif str(variable).lower() in "ti":
-        freestream.set_time_interval(float(value))
+        freestream.set_time_interval(value)
     elif str(variable).lower() in "ts":
-        freestream.set_time_step(float(value))
+        freestream.set_time_step(value)
     elif str(variable).lower() in "rl":
-        freestream.set_reference_length(float(value))
+        freestream.set_reference_length(value)
     elif str(variable).lower() in "nn":
-        freestream.set_number_of_nodes(float(value))
+        freestream.set_number_of_nodes(value)
+    # Reset internal values of class before recomputing
+    freestream.reset_internal_values()
 
 
 # main function
@@ -33,7 +39,13 @@ def main():
         print("What variable would you like to change?")
         print("V, FD, dt, dx, ti, ts, rl, nn")
         choise = raw_input("Choise: ")
-        value = raw_input("Value: ")
-        set_variabel(freestream_calc, choise, value)
+        if str(choise).lower() == "reset":
+            freestream_calc = Freestream()
+        elif str(choise).lower() in ["quit", "exit"]:
+            break
+        else:
+            value = raw_input("Value: ")
+            print("")
+            set_variabel(freestream_calc, choise, value)
 
 main()
